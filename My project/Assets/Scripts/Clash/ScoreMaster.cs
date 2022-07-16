8fde78b2350852329317bd1977b50e478eafd4b0
Braunstein1077 <bryan.reuter@outlook.com>
2022-07-16 17:13:04 +0200

    Clash UI minor changes

---
+++ My project/Assets/Scripts/Clash/ScoreMaster.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class ScoreMaster : MonoBehaviour {
+
+
+public int scoreP1 = 0;
+public int scoreP2 = 0;
+
+    void Update()
+    {
+    }
+}
+
+
