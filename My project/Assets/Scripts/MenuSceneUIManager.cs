477c0912589357f47a47cf2c626478544902d775
Braunstein1077 <bryan.reuter@outlook.com>
2022-07-16 22:29:02 +0200

    Settings V1
    HAHHAHAHHAHA IHR LUSCHEN ICH HABS GESCHAFFT

--- My project/Assets/Scripts/MenuSceneUIManager.cs
+++ My project/Assets/Scripts/MenuSceneUIManager.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
+using TMPro;
 
 
 public class MenuSceneUIManager : MonoBehaviour
 {
+
     public static MenuSceneUIManager Instance { get; private set; }
+
+    public float rolls;
+    public float rounds;
 
     public void Awake()
     {
@@ -16,6 +23,11 @@ public class MenuSceneUIManager : MonoBehaviour
     public void Start()
     {
         InitMenuScene();
+        GameObject.Find("set_main").SetActive(true);
+        GameObject.Find("set_options").SetActive(false);
+
+        rolls = GameObject.Find("sliderRolls").GetComponent<Slider>().value;
+        rounds = GameObject.Find("sliderRounds").GetComponent<Slider>().value;
     }
 
     private void InitMenuScene()
@@ -34,4 +46,18 @@ public class MenuSceneUIManager : MonoBehaviour
         Debug.Log("bye");
         Application.Quit();
     }
+
+    public void Update()
+    {
+        UpdateSliderText();
+    }
+
+    public void UpdateSliderText()
+    {
+        rolls = GameObject.Find("sliderRolls").GetComponent<Slider>().value;
+        GameObject.Find("rolls").GetComponent<TextMeshProUGUI>().text = rolls.ToString();
+        rounds = GameObject.Find("sliderRounds").GetComponent<Slider>().value;
+        GameObject.Find("rounds").GetComponent<TextMeshProUGUI>().text = rounds.ToString();
+    }
+
 }
