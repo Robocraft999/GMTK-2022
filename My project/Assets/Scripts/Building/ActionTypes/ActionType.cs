0d29bef7d3a8c3764711ffd3ab19fdc611794f81
Robocraft999 <Alexakeil987@gmail.com>
2022-07-16 13:20:56 +0200

    updated action types

---
+++ My project/Assets/Scripts/Building/ActionTypes/ActionType.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ActionType : ScriptableObject
+{
+    public string Name;
+    public int Cost;
+}
