1a96060069c42ce530a0cb9415fc0e8b2acc4642
Robocraft999 <Alexakeil987@gmail.com>
2022-07-16 15:51:24 +0200

    added player controller

---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void applyForce(Vector2 force)
+    {
+        rb.AddForce(force);
+    }
+}
