b740d69dca248f44808174567c293bc975090988
Robocraft999 <Alexakeil987@gmail.com>
2022-07-16 15:50:49 +0200

    Changed ActionType

--- My project/Assets/Scripts/Building/ActionTypes/ActionType.cs
+++ My project/Assets/Scripts/Building/ActionTypes/ActionType.cs
@@ -2,10 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 [CreateAssetMenu]
 public class ActionType : ScriptableObject
 {
     public string Name;
     public int Cost;
+    public bool attack;
+    public bool block;
+    public bool applyForce { get => force != Vector2.zero; }
+    public Vector2 force;
 }
