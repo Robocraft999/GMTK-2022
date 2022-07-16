363812d5f1ffb3782c6b18fee57fd2511b4978b6
Robocraft999 <Alexakeil987@gmail.com>
2022-07-16 19:02:02 +0200

    Updated Clash

--- My project/Assets/Scripts/Clash/ScoreMaster.cs
+++ My project/Assets/Scripts/Clash/ScoreMaster.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 
 
-public class ScoreMaster : MonoBehaviour {
+public class ScoreMaster : MonoBehaviour 
+{
+    public TMP_Text scoreTextP1;
+    private int score1 = 0;
+    public int Score1 { 
+        get => score1;
+        set {
+            score1 = value;
+            if (scoreTextP1) scoreTextP1.text = ""+value;
+        }
+    }
 
-
-public int scoreP1 = 0;
-public int scoreP2 = 0;
+    public TMP_Text scoreTextP2;
+    private int score2 = 0;
+    public int Score2
+    {
+        get => score2;
+        set
+        {
+            if (scoreTextP2) scoreTextP2.text = "" + value;
+        }
+    }
 
     void Update()
     {
+        
     }
 }
 
