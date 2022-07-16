e90c3d3561c423568701ddc1b4e62f742f5c3f2e
Robocraft999 <Alexakeil987@gmail.com>
2022-07-16 18:32:23 +0200

    adden ToString methods

--- My project/Assets/Scripts/Building/ActionItem.cs
+++ My project/Assets/Scripts/Building/ActionItem.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class ActionItem : DragItem
 {
     public ActionType Type { get; set; }
+
+    public override string ToString()
+    {
+        return "ActionItem[" + Type.Name + "]";
+    }
 }
--- My project/Assets/Scripts/Building/ActionSlot.cs
+++ My project/Assets/Scripts/Building/ActionSlot.cs
@@ -6,4 +6,9 @@ public class ActionSlot : DragSlot
 {
     public int slotId;
     //public new ActionItem CurrentItem { get; set; }
+
+    public override string ToString()
+    {
+        return "ActionSlot[" + slotId + ", " + CurrentItem + "]";
+    }
 }
